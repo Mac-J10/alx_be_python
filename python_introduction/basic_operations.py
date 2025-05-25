13b1d7a3c1d85d2bf2c9b3541325bb0869c058b9
Jackson <jacksonjay916@gmail.com>
2025-05-25 19:22:48 +0300

    basic python operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+number1 = 10
+number2 = 5
+
+def add_numbers(number1, number2):
+    added = number1 + number2
+    print("Addition of " + str(number1) + " and " + str(number2) + " is: " + str(added))
+
+
+def subtract_numbers(number1, number2):
+    subtracted = number1 - number2
+    print("Subtraction of " + str(number1) + " and " + str(number2) + " is: " + str(subtracted))
+
+def multiply_numbers(number1, number2):
+    multiplied = number1 * number2
+    print("Multiplication of " + str(number1) + " and " + str(number2) + " is: " + str(multiplied))
