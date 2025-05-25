3df511b8bc8f410cf688be9d747a00c20ddf92ec
Jackson <jacksonjay916@gmail.com>
2025-05-25 20:03:05 +0300

    basic operations revised

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -3,13 +3,26 @@ number2 = 5
 
 def add_numbers(number1, number2):
     added = number1 + number2
-    print("Addition of " + str(number1) + " and " + str(number2) + " is: " + str(added))
+    return added
+
+added = add_numbers(number1, number2)
+print("Addition of " + str(number1) + " and " + str(number2) + " is " + str(added))
 
 
 def subtract_numbers(number1, number2):
     subtracted = number1 - number2
-    print("Subtraction of " + str(number1) + " and " + str(number2) + " is: " + str(subtracted))
+    return subtracted
+
+subtracted = subtract_numbers(number1, number2)
+print("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(subtracted))
 
 def multiply_numbers(number1, number2):
     multiplied = number1 * number2
-    print("Multiplication of " + str(number1) + " and " + str(number2) + " is: " + str(multiplied))
+    return multiplied
+
+multiplied = multiply_numbers(number1, number2)
+print("Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(multiplied))
+
+    
+    
+    
---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,5 @@
+length = 10
+width = 5
+
+area = length * width #caculate area of rectangle
+print("The area of the rectangle is: " + str(area))
