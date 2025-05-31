4275d8c331107e5fba9822f0685eaf36028f23c6
Jackson <jacksonjay916@gmail.com>
2025-05-31 19:54:18 +0300

    reviewed match case

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -6,16 +6,16 @@
 def calculator():
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
-    operation = input("Select an operation (addition, subtraction, multiplication, division): ")
+    operation = input("Choose the operation (+, -, *, /): ")
 
     match operation:
-        case "addition":
+        case "+":
             result = num1 + num2
-        case "subtraction":
+        case "-":
             result = num1 - num2
-        case "multiplication":
+        case "*":
             result = num1 * num2
-        case "division":
+        case "/":
             if num2 != 0:
                 result = num1 / num2
             else:
