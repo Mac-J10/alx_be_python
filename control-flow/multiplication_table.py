909b4be03136bbb5236896a8dfa7096cf4b1c016
Jackson <jacksonjay916@gmail.com>
2025-05-31 20:05:47 +0300

    revised mult table

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,9 +1,5 @@
 #Python script named multiplication_table.py using a for loop to print the multiplication table for that number from 1 to 10.
+number = int(input("Enter a number: "))
+for i in range(1, 11):
+    print(f"{number} x {i} = {number * i}")
 
-def print_multiplication_table():
-    number = int(input("Enter a number: "))
-    for i in range(1, 11):
-        print(f"{number} x {i} = {number * i}")
-
-if __name__ == "__main__":
-    print_multiplication_table()
